3eb4cf844d0a21d106f277b7d3cb2f9ca1bf5626
caub <cyril.auburtin@gmail.com>
2017-09-14 09:51:18 +0200

    ES5

--- index.js
+++ index.js
@@ -7,24 +7,26 @@ parse any string to a valid pg tsquery
 module.exports = tsquery;
 
 // todo: should do a proper parser at this point (better for parens)
-function tsquery(q = '') {
+function tsquery(q) {
+	q = q || '';
 	
-	// const q1 = q.replace(/^[\s,()<&+|:]+|\![\s,()<&|:]+|[\s,()<&+|:!]+$|/g, ''); // trim // slower than 2 non global like below
-	const q1 = q.replace(/^[\s,<&+|:]+/, '').replace(/[\s,<&+|:!]+$/, '').replace(/\![\s,<&|:]+/g, ''); // trim
+	var q1 = q.replace(/^[\s,<&+|:]+/, '').replace(/[\s,<&+|:!]+$/, '').replace(/\![\s,<&|:]+/g, ''); // trim
 
-	const q2 = q1.replace(/([\s<&+:!]*[|,])+[\s<&+:]*/g, '|'); // process ORs
+	var q2 = q1.replace(/([\s<&+:!]*[|,])+[\s<&+:]*/g, '|'); // process ORs
 
-	const q3 = q2.replace(/[\s<&+:!]*[\s<&+:]+/g, '&'); // process ANDs
+	var q3 = q2.replace(/[\s<&+:!]*[\s<&+:]+/g, '&'); // process ANDs
 
 	if (q3==='|' || q3==='&') return '';
 
-	const q4 = q3.replace(/(^|[|&])-+(?=\w)/g, '$1!'); // let -word be like !word
+	var q4 = q3.replace(/(^|[|&])-+(?=\w)/g, '$1!'); // let -word be like !word
 
 	// the only operators are now & | !
 	// make sure parens are at the right place, and are balanced
-	const q5 = q4.replace(/[^&|!]([()])(?!([&|!()]|$))/g, a => a.replace(/[()]/g, ''));
-	const re = /[()]/g;
-	let m, balance = 0;
+	var q5 = q4.replace(/[^&|!]([()])(?!([&|!()]|$))/g, function (a) {
+		return a.replace(/[()]/g, '');
+	});
+	var re = /[()]/g;
+	var m, balance = 0;
 	while (m=re.exec(q5)) {
 		balance += m[0]==='(' ? 1 : -1;
 	}
