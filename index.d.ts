e1938ef4a2cc30bb7a0f3fe3725608e8cdf8d4cd
caub <cyril.auburtin@gmail.com>
2019-02-10 09:41:03 +0100

    fix TS #7

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 export interface TsqueryOptions {
-    word: RegExp; // regex for a word. It must contain named captures: word (required), quote (optional), phrase (optional), negated (optional)
-    negated: RegExp; // regex to detect if the expression following an operator is negated, this is useful for example with 'foo!bar', you can parse it as foo&!bar by adding the negation as part of and
-    quotedWordSep: RegExp; // regex for word delimiters inside quotes
+    word?: RegExp; // regex for a word. It must contain named captures: word (required), quote (optional), phrase (optional), negated (optional)
+    negated?: RegExp; // regex to detect if the expression following an operator is negated, this is useful for example with 'foo!bar', you can parse it as foo&!bar by adding the negation as part of and
+    quotedWordSep?: RegExp; // regex for word delimiters inside quotes
     or?: RegExp | undefined; // regex for `or` operator
     and?: RegExp | undefined; // regex for `and` operator
     followedBy?: RegExp | undefined; // regex for `followedBy` operator
@@ -11,4 +11,4 @@ export interface TsqueryOptions {
     tailOp?: string | undefined; // default operator to use with tail (unparsed suffix of the query, if any)
 }
 
-export default function (options: TsqueryOptions): (str: string) => string;
+export default function (options?: TsqueryOptions): (str: string) => string;
