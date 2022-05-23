31aa20ec44fda01988651329e72ecbf4814034ed
caub <cyril.auburtin@gmail.com>
2022-05-23 15:28:05 +0200

    Fix: allow dash in words

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ class Tsquery {
     or = /^\s*(?:[|,]|or)/i,
     and = /^(?!\s*(?:[|,]|or))(?:[\s&+:|,!-]|and)*/i, // /^\s*(?:[\s&+:|,!-]|and)*/i,
     followedBy = /^\s*>/, // /^\s*<(?:(?:(\d+)|-)?>)?/,
-    word = /^[\s*&+<:,|]*(?<negated>[\s!-]*)[\s*&+<:,|]*(?:(?<quote>["'])(?<phrase>.*?)\k<quote>|(?<word>[^\s,|&+<>:*()[\]!-]+))/,
+    word = /^[\s*&+<:,|]*(?<negated>[\s!-]*)[\s*&+<:,|]*(?:(?<quote>["'])(?<phrase>.*?)\k<quote>|(?<word>[^\s,|&+<>:*()[\]!]+))/,
     quotedWordSep = /(?:[\s<()|&!]|:\*)+/, // those are mostly tsquery operator, not removing them would cause errors
     parStart = /^\s*[!-]*[([]/,
     parEnd = /^[)\]]/,
